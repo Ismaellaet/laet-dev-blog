168837f4e69f6933633baf6c40b749f2865b1582
Ismael Abashi <ismaelabashi@hotmail.com>
2024-11-23 20:52:50 -0300

    feat: add link in logo to home page

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,14 +1,17 @@
 import { Logo } from '@components/logo';
 import { ModeToggle } from '@components/theme-toggle';
+import Link from 'next/link';
 
 export const Header = () => {
   return (
     <header className="sticky top-0 z-40 bg-slate-100/90 backdrop-blur dark:bg-slate-900/90 sm:mx-auto sm:w-full sm:max-w-screen-xl">
       <div className="mx-4 flex justify-between border-b py-4">
-        <div className="text-3xl text-primary-foreground">
-          <Logo />
-          <span className="text-secondary-foreground">| Blog</span>
-        </div>
+        <Link href="/">
+          <div className="text-3xl text-primary-foreground">
+            <Logo />
+            <span className="text-secondary-foreground">| Blog</span>
+          </div>
+        </Link>
 
         <ModeToggle />
       </div>
