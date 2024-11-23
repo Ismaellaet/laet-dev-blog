aabe28ca4195a7ea32f924fc5b2dc856749e6f60
Ismael Abashi <ismaelabashi@hotmail.com>
2024-11-23 19:48:29 -0300

    chore: remove unused import

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
 import type { Config } from 'tailwindcss';
 import { fontFamily } from 'tailwindcss/defaultTheme';
-import colors from 'tailwindcss/colors';
 
 export default {
   darkMode: ['class'],
