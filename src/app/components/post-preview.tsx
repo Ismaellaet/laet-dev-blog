3d68d0cc7bcfbe69b1455cf827e8abae3ef53c56
Ismael Abashi <ismaelabashi@hotmail.com>
2024-11-30 12:27:05 -0300

    feat: change tag cursor to default

--- src/app/components/post-preview.tsx
+++ src/app/components/post-preview.tsx
@@ -48,7 +48,7 @@ export const PostPreview = ({
           {tags.map((tag) => (
             <div
               key={tag}
-              className="inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs"
+              className="inline-flex cursor-default items-center rounded-full border px-2.5 py-0.5 text-xs"
             >
               {tag}
             </div>
