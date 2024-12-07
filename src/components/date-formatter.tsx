a8350969c3ced8decbe7cb8dab6672aad1560584
Ismael Abashi <ismaelabashi@hotmail.com>
2024-12-07 13:08:59 -0300

    feat: change date by locale

--- src/components/date-formatter.tsx
+++ src/components/date-formatter.tsx
@@ -1,18 +1,29 @@
-import { format, parseISO } from 'date-fns';
-import { ptBR } from 'date-fns/locale';
+import { format, Locale as LocaleDateFns, parseISO } from 'date-fns';
+import { enUS, ptBR } from 'date-fns/locale';
+import { useLocale } from 'next-intl';
 
 type DateFormatterProps = {
   dateString: string;
 };
 
 export const DateFormatter = ({ dateString }: DateFormatterProps) => {
+  const locale = useLocale();
   const date = parseISO(dateString);
+  const formatByLocale: Record<string, string> = {
+    en: 'MMM d, yyyy',
+    'pt-br': "d 'de' MMM. 'de' yyyy",
+  };
+  const configByLocale: Record<string, LocaleDateFns> = {
+    en: enUS,
+    'pt-br': ptBR,
+  };
+
   return (
     <time
       dateTime={dateString}
       className="text-sm text-secondary-foreground"
     >
-      {format(date, "d 'de' MMM. 'de' yyyy", { locale: ptBR })}
+      {format(date, formatByLocale[locale], { locale: configByLocale[locale] })}
     </time>
   );
 };
